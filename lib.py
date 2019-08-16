706fa15ee21a7a6001a8177da22d7e1de6d2871d
jasonkalmeida <jasonkalmeida@gmail.com>
2019-08-16 00:46:18 -0700

    Working Pull & Timescrape

--- lib.py
+++ lib.py
@@ -1,6 +1,24 @@
 import urllib.request
+import datetime
+import requests
+from bs4 import BeautifulSoup
 
-print('Beginning file download with urllib2...')
+currDate = datetime.datetime.now();
 
-url = 'https://projects.fivethirtyeight.com/polls-page/president_primary_polls.csv'
-urllib.request.urlretrieve(url, 'polls.csv')
+def getFile():
+    print('Beginning file download with urllib2...')
+    url = 'https://projects.fivethirtyeight.com/polls-page/president_primary_polls.csv'
+    urllib.request.urlretrieve(url, currDate.strftime("%m") + '-' + currDate.strftime("%d") + '-polls.csv')
+
+#getFile();
+
+page = requests.get("https://data.fivethirtyeight.com")
+#print(page)
+soup = BeautifulSoup(page.content, 'html.parser')
+#print(soup.prettify())
+
+pollTime = soup.find("table", {"id": "dataIndex"}).find("td", {"id": "polls"}).parent.find("td", {"class": "date"})['datetime']
+
+
+
+print(pollTime)
