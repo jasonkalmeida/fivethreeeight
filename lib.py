9f3544db018afab41a0ce15bd694f8e303a26a6a
jasonkalmeida <jasonkalmeida@gmail.com>
2019-08-22 23:43:06 -0700

    Clean Up Update

--- lib.py
+++ lib.py
@@ -5,21 +5,17 @@ from bs4 import BeautifulSoup
 
 currDate = datetime.datetime.now();
 
+def checkTime():
+    page = requests.get("https://data.fivethirtyeight.com")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    siteTime = soup.find("table", {"id": "dataIndex"}).find("td", {"id": "polls"}).parent.find("td", {"class": "date"})['datetime']
+    pollTime = datetime.datetime
+
+    print(pollTime)
+
 def getFile():
     print('Beginning file download with urllib2...')
     url = 'https://projects.fivethirtyeight.com/polls-page/president_primary_polls.csv'
     urllib.request.urlretrieve(url, currDate.strftime("%m") + '-' + currDate.strftime("%d") + '-polls.csv')
 
-#getFile();
-
-page = requests.get("https://data.fivethirtyeight.com")
-#print(page)
-soup = BeautifulSoup(page.content, 'html.parser')
-#print(soup.prettify())
-
-siteTime = soup.find("table", {"id": "dataIndex"}).find("td", {"id": "polls"}).parent.find("td", {"class": "date"})['datetime']
-
-pollTime = datetime.datetime
-
-
-print(pollTime)
+getFile();
